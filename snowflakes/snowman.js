199d94b462e3731ca4a1d0570bda3c0cc458bcb0
Asaia Palacios <asaia.palacios@gmail.com>
2017-03-01 17:23:45 -0800

    Change ellipse values to render snowman

--- snowflakes/snowman.js
+++ snowflakes/snowman.js
@@ -2,16 +2,16 @@ var snowColor = 255;
 
 function SnowMan() {
   var centerX = screenWidth/2;
-  var centerY = screenHeight/2 + 245;
+  var centerY = screenHeight/2 + 240;
   var offsetHand = 30;
 
-  stroke(210);
+  // stroke(210);
   fill(snowColor);
 
   // Bottom circle (x,y,width,height) 
   ellipse(centerX,625,100,100);
   ellipse(centerX,centerY,80,80);
-  ellipse(centerX,550,60,60);
+  ellipse(centerX,500,60,60);
   
   line(centerX-100,centerY-offsetHand,centerX-40,centerY - 15);
   line(centerX+100,centerY-offsetHand,centerX+40,centerY - 15);
